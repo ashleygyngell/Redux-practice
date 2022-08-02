7ed5ef1a39ccd74b66a21e1fef29cca81a3b6172
Ashley Gyngell <ashleygyngell@gmail.com>
2022-08-02 22:59:50 +0100

    Fixed counter! First time meeting redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,57 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// // STORE -> Globalised State
+
+// // ACTION INCREMENT
+
+// const incrament = () => {
+//   return {
+//     type: 'INCRAMENT',
+//   };
+// };
+
+// const decrament = () => {
+//   return {
+//     type: 'DECRAMENT',
+//   };
+// };
+
+// // REDUCER
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCRAMENT':
+//       return state + 1;
+//     case 'DECRAMENT':
+//       return state - 1;
+//   }
+// };
+
+// let store = createStore(counter);
+
+// // Display it in the console
+// store.subscribe(() => console.log(store.getState()));
+
+// // DISPATCH
+// store.dispatch(incrament());
+// store.dispatch(incrament());
+// store.dispatch(decrament());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
